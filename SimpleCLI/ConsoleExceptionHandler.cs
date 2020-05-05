93d8301e6ad1e4ac410bfffc153b90c68c879f56
Hu Yao <hooyao@users.noreply.github.com>
2020-05-05 13:26:35 +0800

    Implement stream read-decipher-lz4decode (#2)
    * Implement Stream reader and writer
    * Update README.md
    
    Use stream to read and write file, reduce memory footprint.
    
    TODO
    * Unit tests
    * Refactor code to extend to other file sources
    * Use ring buffer to fix performance issue in *CloudSyncPayloadStream*

---
+++ SimpleCLI/ConsoleExceptionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using com.hy.synology.filemanager.core.exception;
+
+namespace com.hy.synolocgy.filemanager.simplecli
+{
+    public class ConsoleExceptionHandler:IExceptionHandler
+    {
+        public void Handle(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
